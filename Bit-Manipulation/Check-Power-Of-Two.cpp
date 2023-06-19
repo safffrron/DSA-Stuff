23e42b51fa0456012ae5f6c27e09aceb211e0ddf
Kislay Aditya Oj <125828335+safffrron@users.noreply.github.com>
2023-06-19 15:16:49 +0530

    Create Check-Power-Of-Two.cpp

---
+++ Bit-Manipulation/Check-Power-Of-Two.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define end '\n'
+using namespace std;
+
+int CheckPowerOfTwo(int n)
+{
+    return !(n&(n-1));
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    //input test cases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    
+    return 0;
+}  
