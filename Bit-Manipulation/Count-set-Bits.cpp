41170301934e59d2f284fdbba57a3d8a2cd23d87
Kislay Aditya Oj <125828335+safffrron@users.noreply.github.com>
2023-06-19 15:19:09 +0530

    Create Count-set-Bits.cpp

---
+++ Bit-Manipulation/Count-set-Bits.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define end '\n'
+using namespace std;
+
+int countSetBits(int n)
+{
+  int c=0;
+      while(n>0)
+      {
+          n=n&(n-1);
+          c++;
+      }
+  return c;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    //input test cases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    
+    return 0;
+}  
