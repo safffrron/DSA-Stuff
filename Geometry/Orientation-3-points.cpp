6d1c71651532c3cb2779e764fb5b8e2bc1d77a37
Kislay Aditya Oj <125828335+safffrron@users.noreply.github.com>
2023-06-20 21:07:47 +0530

    Create Orientation-3-points.cpp

---
+++ Geometry/Orientation-3-points.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define end '\n'
+using namespace std;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+int solve(Point p1,Point p2, Point p3)
+{
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    //input test cases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+    }
+    
+    return 0;
+} 
